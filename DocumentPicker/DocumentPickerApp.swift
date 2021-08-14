45c97806d1196a6d929aa32ab6dd14f3fa422176
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-08-14 12:37:06 +0200

    Initial Commit

---
+++ DocumentPicker/DocumentPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocumentPickerApp.swift
+//  DocumentPicker
+//
+//  Created by Michele Manniello on 14/08/21.
+//
+
+import SwiftUI
+
+@main
+struct DocumentPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
